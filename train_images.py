66e041ce03d73f8b231ca88cd64e3b97dd89fd68
Sherlock Lee <pli258@wisc.edu>
2023-06-27 13:16:35 -0500

    Encode Built; Utils Added
    model_details: the details about the model
    model_build_test.py: test the model

--- train_images.py
+++ train_images.py
@@ -21,16 +21,18 @@ from utils.models import ResNet_encdec
 from utils.data_loader import image_dataset
 import sys
 from pathlib import Path
-
-
+from utils.models import *
+import datetime
+from torchinfo import summary
+import math
 ########################################## <Config> ############################################
 # Parse Configuration
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', type=str, default='data/imaging_data/(256, 256, 156)')
-parser.add_argument('--output_file', type=str, default='experiments/imaging_data/(256, 256, 156)')
+parser.add_argument('--file', type=str, default='data/imaging_data/(157, 189, 156)')
+parser.add_argument('--output_file', type=str, default='experiments/imaging_data/(157, 189, 156)')
 parser.add_argument('--latent_dim', type=int, default=10)
 parser.add_argument('--epochs', type=int, default=100)
-parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--morph_loss_factor', type=float, default=1)
 parser.add_argument('--seed', type=int, default=42)
@@ -38,7 +40,9 @@ parser.add_argument('--seed', type=int, default=42)
 parser.add_argument('--equivariant_variables', nargs='+', type=str, default=['sex', 'Age quant'])
 parser.add_argument('--age_bin', type=float, default=10)
 parser.add_argument("--source", type = str, default = "")
+parser.add_argument("--num_warmup_steps", type = int, default = 0)
 args = parser.parse_args() # parse arguments
+
 
 # Save parameters
 try:
@@ -58,7 +62,6 @@ random.seed(args.seed)
 np.random.seed(args.seed)
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-# device = torch.device("cpu")
 print(f'Selected device: {device}')
 
 
@@ -122,221 +125,265 @@ test_dataloader = DataLoader(test_data,
 
 
 ############################################ <Model> ############################################
-latent_dim = args.latent_dim
 
-model = ResNet_encdec(latent_dim=latent_dim).to(device)
-up_matrices = [Variable(torch.normal(mean=0, std=0.1, size=(latent_dim, latent_dim)).to(device), requires_grad=True) for _ in range(len(args.equivariant_variables))]
-down_matrices = [Variable(torch.normal(mean=0, std=0.1, size=(latent_dim, latent_dim)).to(device), requires_grad=True) for _ in range(len(args.equivariant_variables))]
 
-loss_mse = torch.nn.MSELoss()
-optimizer = optim.Adam([{'params': model.parameters()}] +
-					   [{'params': up_matrix} for up_matrix in up_matrices] +
-					   [{'params': down_matrix} for down_matrix in down_matrices], lr=args.lr)
-# # ############################################ </Model> ############################################
-#
-# train_stats = {'train_loss': [],
-# 			   'train_recon_loss': [],
-# 			   'train_morph_loss': [],
-# 			   'train_inv_loss': [],
-# 			   'val_loss': [],
-# 			   'val_recon_loss': [],
-# 			   'val_morph_loss': []}
-#
-#
-# max_epochs = args.epochs
-# for epoch in range(max_epochs):
-#
-# 	# Train
-# 	model.train()
-#
-#
-# 	total_loss = 0
-# 	total_recon_loss = 0
-# 	total_morph_loss = 0
-# 	total_inv_loss = 0
-#
-#
-# 	progress_bar = tqdm(train_generator, total=len(train_generator))
-# 	for batch in progress_bar:
-#
-#
-# 		X = batch[0].to(device)
-# 		mdata = batch[1]
-#
-# 		X_out, _, latent = model(X)
-#
-# 		recon_loss = loss_mse(X, X_out)
-#
-#
-# 		# Morphisms preserving
-# 		morph_loss = 0
-#
-# 		matrices_powers = []
-# 		differences = []
-#
-# 		for variable_index in range(len(args.equivariant_variables)):
-#
-# 			matrices_powers.append({0: torch.eye(latent_dim).to(device)})
-# 			differences.append((np.repeat(np.array(mdata[:, variable_index]).reshape((-1, 1)), mdata.shape[0], 1) - np.array(mdata[:, variable_index])).astype(int))
-#
-#
-# 			max_pos_dif = differences[variable_index].max()
-# 			max_neg_dif = -differences[variable_index].min()
-#
-# 			for p in range(1, max_neg_dif+1):
-# 				matrices_powers[variable_index][p] = torch.linalg.matrix_power(down_matrices[variable_index], p)
-# 			for p in range(1, max_pos_dif+1):
-# 				matrices_powers[variable_index][-p] = torch.linalg.matrix_power(up_matrices[variable_index], p)
-#
-#
-#
-#
-# 		for i in range(mdata.shape[0]):
-# 			for j in range(i+1, mdata.shape[0]):
-#
-# 				current_matrix = torch.eye(latent_dim).to(device)
-# 				for variable_index in range(len(args.equivariant_variables)):
-# 					current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][i, j]]
-#
-# 				latent_mul = torch.matmul(latent[i], current_matrix)
-# 				# Maybe cs would be better
-# 				morph_loss += loss_mse(latent[j], latent_mul)
-#
-#
-#
-# 				current_matrix = torch.eye(latent_dim).to(device)
-# 				for variable_index in range(len(args.equivariant_variables)):
-# 					current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][j, i]]
-#
-# 				latent_mul = torch.matmul(latent[j], current_matrix)
-# 				# Maybe cs would be better
-# 				morph_loss += loss_mse(latent[i], latent_mul)
-#
-#
-#
-#
-# 		morph_loss /= X.shape[0]**2
-#
-# 		# Inverse loss
-# 		inv_loss = 0
-# 		for i in range(len(args.equivariant_variables)):
-# 			inv_loss += loss_mse(torch.matmul(up_matrices[i], down_matrices[i]), torch.eye(args.latent_dim).to(device))
-# 			inv_loss += loss_mse(torch.matmul(down_matrices[i], up_matrices[i]), torch.eye(args.latent_dim).to(device))
-#
-#
-# 		# Update parameters
-# 		loss = recon_loss + args.morph_loss_factor*morph_loss + 1*inv_loss
-#
-# 		# Keep stats
-# 		total_loss += loss.item()
-# 		total_recon_loss += recon_loss.item()
-# 		total_morph_loss += morph_loss.item()
-# 		total_inv_loss += inv_loss.item()
-#
-# 		optimizer.zero_grad()
-# 		loss.backward()
-# 		optimizer.step()
-#
-# 		progress_bar.set_description("%3d/%3d - Train loss: %.4f (Recon: %.4f, Morph: %.8f, Inv: %.2f)" % (epoch+1, max_epochs, loss, recon_loss, morph_loss, inv_loss))
-#
-#
-#
-# 	train_stats['train_loss'].append(total_loss/len(train_generator))
-# 	train_stats['train_recon_loss'].append(total_recon_loss/len(train_generator))
-# 	train_stats['train_morph_loss'].append(total_morph_loss/len(train_generator))
-# 	train_stats['train_inv_loss'].append(total_inv_loss/len(train_generator))
-#
-#
-# 	# Eval
-# 	model.eval()
-#
-# 	with torch.no_grad():
-#
-#
-# 		recon_loss = 0
-# 		morph_loss = 0
-#
-#
-#
-# 		progress_bar = tqdm(val_generator, total=len(val_generator))
-# 		for batch in progress_bar:
-#
-#
-# 			X = batch[0].to(device)
-# 			mdata = batch[1]
-#
-# 			X_out, _, latent = model(X)
-#
-#
-# 			recon_loss += loss_mse(X, X_out)
-#
-#
-# 			# Morphisms preserving
-# 			matrices_powers = []
-# 			differences = []
-#
-# 			for variable_index in range(len(args.equivariant_variables)):
-#
-# 				matrices_powers.append({0: torch.eye(latent_dim).to(device)})
-# 				differences.append((np.repeat(np.array(mdata[:, variable_index]).reshape((-1, 1)), mdata.shape[0], 1) - np.array(mdata[:, variable_index])).astype(int))
-#
-#
-# 				max_pos_dif = differences[variable_index].max()
-# 				max_neg_dif = -differences[variable_index].min()
-#
-# 				for p in range(1, max_neg_dif+1):
-# 					matrices_powers[variable_index][p] = torch.linalg.matrix_power(down_matrices[variable_index], p)
-# 				for p in range(1, max_pos_dif+1):
-# 					matrices_powers[variable_index][-p] = torch.linalg.matrix_power(up_matrices[variable_index], p)
-#
-#
-#
-#
-# 			for i in range(mdata.shape[0]):
-# 				for j in range(i+1, mdata.shape[0]):
-#
-# 					current_matrix = torch.eye(latent_dim).to(device)
-# 					for variable_index in range(len(args.equivariant_variables)):
-# 						current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][i, j]]
-#
-# 					latent_mul = torch.matmul(latent[i], current_matrix)
-# 					# Maybe cs would be better
-# 					morph_loss += loss_mse(latent[j], latent_mul)
-#
-#
-#
-# 					current_matrix = torch.eye(latent_dim).to(device)
-# 					for variable_index in range(len(args.equivariant_variables)):
-# 						current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][j, i]]
-#
-# 					latent_mul = torch.matmul(latent[j], current_matrix)
-# 					# Maybe cs would be better
-# 					morph_loss += loss_mse(latent[i], latent_mul)
-#
-#
-# 			morph_loss /= X.shape[0]**2
-#
-#
-#
-# 		recon_loss /=len(val_generator)
-# 		morph_loss /=len(val_generator)
-#
-# 		print (" Recon: %.4f, Morph: %.8f" % (recon_loss, morph_loss))
-#
-#
-# 		train_stats['val_loss'].append(recon_loss.item() + args.morph_loss_factor*morph_loss.item())
-# 		train_stats['val_recon_loss'].append(recon_loss.item())
-# 		train_stats['val_morph_loss'].append(morph_loss.item())
-#
-#
-#
-# 	torch.save(model.state_dict(), args.output_file + '/models/model_%d' %(epoch))
-#
-# 	for i, up_matrix in enumerate(up_matrices):
-# 		torch.save(up_matrix, args.output_file + '/models/up_matrix_%d_%d' %(i, epoch))
-#
-# 	for i, down_matrix in enumerate(down_matrices):
-# 		torch.save(down_matrix, args.output_file + '/models/down_matrix_%d_%d' %(i, epoch))
-#
-# train_stats_pd = pd.DataFrame.from_dict(train_stats)
-# train_stats_pd.to_csv(args.output_file + '/train_stats.csv')
+					################# Vishnu's Version #################
+# Vishnu's Version
+# latent_dim = args.latent_dim
+#
+# model = ResNet_encdec(latent_dim=latent_dim).to(device)
+# up_matrices = [Variable(torch.normal(mean=0, std=0.1, size=(latent_dim, latent_dim)).to(device), requires_grad=True) for _ in range(len(args.equivariant_variables))]
+# down_matrices = [Variable(torch.normal(mean=0, std=0.1, size=(latent_dim, latent_dim)).to(device), requires_grad=True) for _ in range(len(args.equivariant_variables))]
+
+# loss_mse = torch.nn.MSELoss()
+# optimizer = optim.Adam([{'params': model.parameters()}] +
+# 					   [{'params': up_matrix} for up_matrix in up_matrices] +
+# 					   [{'params': down_matrix} for down_matrix in down_matrices], lr=args.lr)
+
+
+                    ################# Peiran's Version #################
+model = Encoder(conv_in_features = 1, conv_out_features = 128, kernel_size = 3, padding = True,
+				batch_norm = True, img_size = 256, in_channels = 8, patch_size = 16, batch_size =
+				32, num_transformer_layer = 24, embedding_dim = 2048, mlp_size = 4096, num_heads
+				= 16, attention_dropout = .0, mlp_dropout = .1, embedding_dropout = .1)
+
+summary(model, input_size = (1, 1, 157, 189, 156),
+		col_names = ["input_size", "output_size", "num_params", "trainable"],
+		col_width = 20,
+		row_settings = ["var_names"])
+
+loss_fn = nn.MSELoss()
+
+optimizer = optim.Adam(model.parameters(), lr = args.lr)
+
+num_training_steps = args.epochs * len(train_dataloader)  # assuming train_dataloader is your DataLoader
+warmup_steps = args.num_warmup_steps
+
+# Define a function that will be used to adjust the learning rate
+# exponential warmup and decay
+def lr_lambda(current_step: int):
+    if current_step < warmup_steps:
+        return math.exp(current_step / warmup_steps) - 1
+    else:
+        return math.exp((warmup_steps - current_step) / (num_training_steps - warmup_steps))
+
+scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda)
+
+########################################## <Training> ############################################
+
+#### You can direct use utils.train_test_utils.train() ####
+# first create a Tensorboard writer by
+# writer = create_writer(experiment_name, model_name, extra_annotations)
+# then call train() by
+# results = train(model, train_dataloader, test_dataloader, optimizer, scheduler, loss_fn, epochs,
+# device, writer = writer)
+# where the "results" is the last expeiement results, which is a dictionary with keys
+# you can directly draw the results by calling plot_loss_curves(results)
+
+
+train_stats = {'train_loss': [],
+			   'train_recon_loss': [],
+			   'train_morph_loss': [],
+			   'train_inv_loss': [],
+			   'val_loss': [],
+			   'val_recon_loss': [],
+			   'val_morph_loss': []}
+
+
+max_epochs = args.epochs
+for epoch in range(max_epochs):
+
+	# Train
+	model.train()
+
+
+	total_loss = 0
+	total_recon_loss = 0
+	total_morph_loss = 0
+	total_inv_loss = 0
+
+
+	progress_bar = tqdm(train_generator, total=len(train_generator))
+	for batch in progress_bar:
+
+
+		X = batch[0].to(device)
+		mdata = batch[1]
+
+		X_out, _, latent = model(X)
+
+		recon_loss = loss_mse(X, X_out)
+
+
+		# Morphisms preserving
+		morph_loss = 0
+
+		matrices_powers = []
+		differences = []
+
+		for variable_index in range(len(args.equivariant_variables)):
+
+			matrices_powers.append({0: torch.eye(latent_dim).to(device)})
+			differences.append((np.repeat(np.array(mdata[:, variable_index]).reshape((-1, 1)), mdata.shape[0], 1) - np.array(mdata[:, variable_index])).astype(int))
+
+
+			max_pos_dif = differences[variable_index].max()
+			max_neg_dif = -differences[variable_index].min()
+
+			for p in range(1, max_neg_dif+1):
+				matrices_powers[variable_index][p] = torch.linalg.matrix_power(down_matrices[variable_index], p)
+			for p in range(1, max_pos_dif+1):
+				matrices_powers[variable_index][-p] = torch.linalg.matrix_power(up_matrices[variable_index], p)
+
+
+
+
+		for i in range(mdata.shape[0]):
+			for j in range(i+1, mdata.shape[0]):
+
+				current_matrix = torch.eye(latent_dim).to(device)
+				for variable_index in range(len(args.equivariant_variables)):
+					current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][i, j]]
+
+				latent_mul = torch.matmul(latent[i], current_matrix)
+				# Maybe cs would be better
+				morph_loss += loss_mse(latent[j], latent_mul)
+
+
+
+				current_matrix = torch.eye(latent_dim).to(device)
+				for variable_index in range(len(args.equivariant_variables)):
+					current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][j, i]]
+
+				latent_mul = torch.matmul(latent[j], current_matrix)
+				# Maybe cs would be better
+				morph_loss += loss_mse(latent[i], latent_mul)
+
+
+
+
+		morph_loss /= X.shape[0]**2
+
+		# Inverse loss
+		inv_loss = 0
+		for i in range(len(args.equivariant_variables)):
+			inv_loss += loss_mse(torch.matmul(up_matrices[i], down_matrices[i]), torch.eye(args.latent_dim).to(device))
+			inv_loss += loss_mse(torch.matmul(down_matrices[i], up_matrices[i]), torch.eye(args.latent_dim).to(device))
+
+
+		# Update parameters
+		loss = recon_loss + args.morph_loss_factor*morph_loss + 1*inv_loss
+
+		# Keep stats
+		total_loss += loss.item()
+		total_recon_loss += recon_loss.item()
+		total_morph_loss += morph_loss.item()
+		total_inv_loss += inv_loss.item()
+
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+		progress_bar.set_description("%3d/%3d - Train loss: %.4f (Recon: %.4f, Morph: %.8f, Inv: %.2f)" % (epoch+1, max_epochs, loss, recon_loss, morph_loss, inv_loss))
+
+
+
+	train_stats['train_loss'].append(total_loss/len(train_generator))
+	train_stats['train_recon_loss'].append(total_recon_loss/len(train_generator))
+	train_stats['train_morph_loss'].append(total_morph_loss/len(train_generator))
+	train_stats['train_inv_loss'].append(total_inv_loss/len(train_generator))
+
+
+	# Eval
+	model.eval()
+
+	with torch.no_grad():
+
+
+		recon_loss = 0
+		morph_loss = 0
+
+
+
+		progress_bar = tqdm(val_generator, total=len(val_generator))
+		for batch in progress_bar:
+
+
+			X = batch[0].to(device)
+			mdata = batch[1]
+
+			X_out, _, latent = model(X)
+
+
+			recon_loss += loss_mse(X, X_out)
+
+
+			# Morphisms preserving
+			matrices_powers = []
+			differences = []
+
+			for variable_index in range(len(args.equivariant_variables)):
+
+				matrices_powers.append({0: torch.eye(latent_dim).to(device)})
+				differences.append((np.repeat(np.array(mdata[:, variable_index]).reshape((-1, 1)), mdata.shape[0], 1) - np.array(mdata[:, variable_index])).astype(int))
+
+
+				max_pos_dif = differences[variable_index].max()
+				max_neg_dif = -differences[variable_index].min()
+
+				for p in range(1, max_neg_dif+1):
+					matrices_powers[variable_index][p] = torch.linalg.matrix_power(down_matrices[variable_index], p)
+				for p in range(1, max_pos_dif+1):
+					matrices_powers[variable_index][-p] = torch.linalg.matrix_power(up_matrices[variable_index], p)
+
+
+
+
+			for i in range(mdata.shape[0]):
+				for j in range(i+1, mdata.shape[0]):
+
+					current_matrix = torch.eye(latent_dim).to(device)
+					for variable_index in range(len(args.equivariant_variables)):
+						current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][i, j]]
+
+					latent_mul = torch.matmul(latent[i], current_matrix)
+					# Maybe cs would be better
+					morph_loss += loss_mse(latent[j], latent_mul)
+
+
+
+					current_matrix = torch.eye(latent_dim).to(device)
+					for variable_index in range(len(args.equivariant_variables)):
+						current_matrix = current_matrix @ matrices_powers[variable_index][differences[variable_index][j, i]]
+
+					latent_mul = torch.matmul(latent[j], current_matrix)
+					# Maybe cs would be better
+					morph_loss += loss_mse(latent[i], latent_mul)
+
+
+			morph_loss /= X.shape[0]**2
+
+
+
+		recon_loss /=len(val_generator)
+		morph_loss /=len(val_generator)
+
+		print (" Recon: %.4f, Morph: %.8f" % (recon_loss, morph_loss))
+
+
+		train_stats['val_loss'].append(recon_loss.item() + args.morph_loss_factor*morph_loss.item())
+		train_stats['val_recon_loss'].append(recon_loss.item())
+		train_stats['val_morph_loss'].append(morph_loss.item())
+
+
+
+	torch.save(model.state_dict(), args.output_file + '/models/model_%d' %(epoch))
+
+	for i, up_matrix in enumerate(up_matrices):
+		torch.save(up_matrix, args.output_file + '/models/up_matrix_%d_%d' %(i, epoch))
+
+	for i, down_matrix in enumerate(down_matrices):
+		torch.save(down_matrix, args.output_file + '/models/down_matrix_%d_%d' %(i, epoch))
+
+train_stats_pd = pd.DataFrame.from_dict(train_stats)
+train_stats_pd.to_csv(args.output_file + '/train_stats.csv')
--- utils/models.py
+++ utils/models.py
@@ -112,12 +112,6 @@ class Decoder(nn.Module):
 		return x
 
 
-
-
-
-
-
-
 class Encoder32(nn.Module):
 
 	def __init__(self, num_input_channels=3, base_channel_size=32, latent_dim=256):
@@ -1185,3 +1179,262 @@ class Discriminator(nn.Module):
 		x = torch.sigmoid(x)
 
 		return x
+
+class Encoder(nn.Module):
+	def __init__(self,
+				 conv_in_features: int = 1,
+				 conv_out_features: int = 128,
+				 kernel_size: int = 3,
+				 padding: bool = True,
+				 batch_norm: bool = True,
+				 img_size: int = 256,
+				 in_channels: int = 8,
+				 patch_size: int = 16,
+				 batch_size: int = 32,
+				 num_transformer_layer: int = 24,
+				 embedding_dim: int = 2048,
+				 mlp_size: int = 4096,
+				 num_heads: int = 16,
+				 attention_dropout: float = .0,
+				 mlp_dropout: float = .1,
+				 embedding_dropout: float = .1):
+		super().__init__()
+		self.conv_block = Consecutive3DConvLayerBlock(in_channel = conv_in_features,
+													  out_channel = conv_out_features,
+													  kernel_size = kernel_size,
+													  padding = padding,
+													  batch_norm = batch_norm)
+		self.vit_block = ViTEncoder(img_size = img_size,
+									in_channels = in_channels,
+									patch_size = patch_size,
+									batch_size = batch_size,
+									num_transformer_layer = num_transformer_layer,
+									embedding_dim = embedding_dim,
+									mlp_size = mlp_size,
+									num_heads = num_heads,
+									attention_dropout = attention_dropout,
+									mlp_dropout = mlp_dropout,
+									embedding_dropout = embedding_dropout)
+
+	def forward(self, x):
+		return self.vit_block(self.conv_block(x)) # add speed
+
+class ViTEncoder(nn.Module):
+	def __init__(self,
+				 img_size: int = 256,
+				 in_channels: int = 8,
+				 patch_size: int = 16,
+				 batch_size: int = 32,
+				 num_transformer_layer: int = 24,
+				 embedding_dim: int = 2048,
+				 mlp_size: int = 4096,
+				 num_heads: int = 16,
+				 attention_dropout: float = .0,
+				 mlp_dropout: float = .1,
+				 embedding_dropout: float = .1):
+		super().__init__()
+
+		assert img_size % patch_size == 0, f"Input Size: {img_size} must be divisible by Patch " \
+										   f"Size: {patch_size}"
+
+		self.num_patches = (img_size * img_size) // (patch_size ** 2)
+
+		self.position_embedding = PositionEmbedding(batch_size = batch_size,
+													num_patches = self.num_patches,
+													embedding_dimension = embedding_dim)
+
+		self.embedding_dropout = nn.Dropout(p = embedding_dropout)
+
+		self.patch_embedding = PatchEmbedding(in_channels = in_channels,
+											  patch_size = patch_size,
+											  embedding_dim = embedding_dim)
+
+		self.transformer_encoder = nn.Sequential(*[TransformerEncoder(embedding_dim =
+																	  embedding_dim,
+																	  num_heads = num_heads,
+																	  mlp_size = mlp_size,
+																	  mlp_dropout = mlp_dropout,
+																	  attention_dropout =
+																	  attention_dropout) for _
+												 in range(num_transformer_layer)])
+
+		self.latent_space = nn.Sequential(nn.LayerNorm(normalized_shape = embedding_dim),
+										  nn.Linear(in_features = embedding_dim,
+													out_features = embedding_dim))
+
+	def forward(self, x):
+		x = self.patch_embedding(x)
+		x = self.position_embedding(x)
+		x = self.embedding_dropout(x)
+		x = self.transformer_encoder(x)
+		x = self.latent_space(x)
+		return x
+
+	def _get_name(self):
+		return "Vision Transformer Encoder"
+
+class Consecutive3DConvLayerBlock(nn.Module):
+	def __init__(self,
+				 in_channel: int = 1,
+				 out_channel: int = 128,
+				 kernel_size: int = 3,
+				 padding: bool = True,
+				 batch_norm: bool = True) -> None:
+		super().__init__()
+		self.in_channel = in_channel
+		self.out_channel = out_channel
+		self.kernel_size = kernel_size
+		self.padding = padding
+		self.batch_norm = batch_norm
+
+
+		self.conv1 = nn.Conv3d(in_channels=self.in_channel,
+							   out_channels = 32,
+							   kernel_size = (self.kernel_size, self.kernel_size,
+											  self.kernel_size),
+							  stride = self.kernel_size)
+
+		self.conv2 = nn.Conv3d(32, 64, (3, 3, 3), padding = 1, stride = 2)
+		self.conv3 = nn.Conv3d(64, self.out_channel, (3, 3, 3), padding = 1, stride = 2)
+
+	def forward(self, x):
+		self.batch_size, self.channels = x.shape[0], x.shape[1]
+		if self.padding:
+			x = _pad_3D_image_patches_with_channel(x, [x.shape[0], x.shape[1], 158, 189, 156])
+		x = self.conv1(x)
+		if self.padding:
+			x = _pad_3D_image_patches_with_channel(x, [x.shape[0], x.shape[1], 64, 64, 64])
+		return self.conv3(self.conv2(x)).reshape(32, -1, 256, 256)
+
+	def _get_name(self):
+		return "3D Conv Layers"
+
+class PatchEmbedding(nn.Module):
+	def __init__(self,
+				 in_channels: int = 8,
+				 patch_size: int = 16,
+				 embedding_dim: int = 2048):
+		super().__init__()
+		self.patcher = nn.Conv2d(in_channels = in_channels,
+								 out_channels = embedding_dim,
+								 kernel_size = patch_size,
+								 stride = patch_size,
+								 padding = 0)
+
+		self.flatten = nn.Flatten(start_dim = 2, end_dim = 3)
+
+		self.patch_size = patch_size
+
+	def forward(self, x):
+		assert x.shape[-1] % self.patch_size == 0, f"[ERROR] Input Resolution Must be Divisible " \
+												   f"by Patch Size. \nImage Shape: " \
+												   f"{x.shape[-1]}\nPatch Size: {self.patch_size}"
+
+		x = self.patcher(x)
+		x = self.flatten(x)
+		return x.permute(0, 2, 1)
+
+
+class PositionEmbedding(nn.Module):
+	def __init__(self,
+				 batch_size: int = 32,
+				 num_patches: int = 256,
+				 embedding_dimension: int = 2048):
+		super().__init__()
+		self.position_matrix = nn.Parameter(torch.randn(batch_size, num_patches,
+													   embedding_dimension),
+									   requires_grad = True)
+
+	def forward(self, x):
+		return x + self.position_matrix
+
+class MultiheadSelfAttention(nn.Module):
+	def __init__(self,
+				 embedding_dim: int = 2048,
+				 num_heads: int = 16,
+				 attention_dropout: float = .0):
+		super().__init__()
+		self.layer_norm = nn.LayerNorm(normalized_shape = embedding_dim)
+		self.multihead_attention = nn.MultiheadAttention(embed_dim = embedding_dim,
+														 num_heads = num_heads,
+														 dropout = attention_dropout,
+														 batch_first = True)
+
+	def forward(self, x):
+		x = self.layer_norm(x)
+		attention_output, _ = self.multihead_attention(query = x,
+													   key = x,
+													   value = x,
+													   need_weights = False)
+		return attention_output
+
+class MultiLayerPerception(nn.Module):
+	def __init__(self,
+				 embedding_dim: int = 2048,
+				 mlp_size: int = 4096,
+				 mlp_dropout: float = 0.1):
+		super().__init__()
+
+		self.layer_norm = nn.LayerNorm(normalized_shape = embedding_dim)
+		self.mlp = nn.Sequential(
+				nn.Linear(in_features = embedding_dim, out_features = mlp_size),
+				nn.GELU(),
+				nn.Dropout(p = mlp_dropout),
+				nn.Linear(in_features = mlp_size, out_features = embedding_dim),
+				nn.Dropout(p = mlp_dropout)
+				)
+
+	def forward(self, x):
+		return self.mlp(self.layer_norm(x))
+
+class TransformerEncoder(nn.Module):
+	def __init__(self,
+				 embedding_dim: int = 2048,
+				 num_heads: int = 16,
+				 mlp_size: int = 4096,
+				 mlp_dropout: float = .1,
+				 attention_dropout: float = .0):
+		super().__init__()
+		self.msa_block = MultiheadSelfAttention(embedding_dim = embedding_dim,
+												num_heads =  num_heads,
+												attention_dropout = attention_dropout)
+		self.mlp_block = MultiLayerPerception(embedding_dim = embedding_dim,
+											  mlp_size = mlp_size,
+											  mlp_dropout = mlp_dropout)
+
+	def forward(self, x):
+		# Create residual connection for MSA block
+		x = self.msa_block(x) + x
+		# Create residual connection for MLP block
+		x = self.mlp_block(x) + x
+		return x
+
+def _pad_3D_image_patches_with_channel(img, desired_size):
+	# Make sure your image tensor has 5 dimensions: (batch_size, channel, dim1, dim2, dim3)
+	# If channel is missing add a dimension for it
+	if len(img.shape) == 4:
+		img = img.unsqueeze(1)  # Add the channel dimension
+
+	# The difference between the desired size and the original size
+	diff = [desired_size[i] - img.size(i) for i in
+			range(-3, 0)]  # -3, -2, -1 correspond to dim1, dim2, dim3
+	diff = diff[::-1]
+	# Prepend with zeros for batch and channel dimensions
+	padding = ()
+	for i in diff:
+		if i % 2 == 0:
+			padding += (i // 2, i // 2)
+		else:
+			padding += (i // 2, i // 2 + 1)
+
+	padding += (0, 0, 0, 0)
+
+	print("Padding:", padding)
+	padded = F.pad(img, padding)
+
+	return padded
+
+
+
+
+
